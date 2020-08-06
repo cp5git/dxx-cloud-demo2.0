29a6db4ec667f6073f619ab555f89ab359bc890d
cp5git <69034851+cp5git@users.noreply.github.com>
2020-08-06 10:03:02 +0800

    producer1添加配日志配置

--- producer1/src/main/java/com/dxx/producer1/controller/ProducerController.java
+++ producer1/src/main/java/com/dxx/producer1/controller/ProducerController.java
@@ -5,14 +5,19 @@ import com.dxx.common.bean.CommonResponse;
 import com.dxx.common.contants.ResponseCode;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Api(tags = "生产者接口")
 public class ProducerController {
 
-    @RequestMapping("/u/user/id")
+    private static final Logger logger = LoggerFactory.getLogger(ProducerController.class);
+
+    @RequestMapping(value="/u/user/id",method = RequestMethod.GET)
     @ApiOperation("获取用户ID接口")
     public CommonResponse userId(){
         return new CommonResponse(ResponseCode.SUCCESS,AuthUtil.getAuth().getUserId());
@@ -25,6 +30,9 @@ public class ProducerController {
 
     @RequestMapping("/test")
     public CommonResponse test(){
+
+        logger.info("test log");
+
         return new CommonResponse(ResponseCode.SUCCESS,"producer 1");
     }
 }
